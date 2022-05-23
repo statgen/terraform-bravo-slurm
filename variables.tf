413e1e0bf2682e807b7c8edd19726ff6ffe76925
Colin Gross <grosscol@gmail.com>
2022-05-23 18:29:43 -0400

    default to use debian image for nodes

--- variables.tf
+++ variables.tf
@@ -26,14 +26,22 @@ variable "project" {
 variable "cluster_name" {}
 
 variable "controller_machine_type" {}
-variable "controller_image"        {}
+variable "controller_image"        {
+  description = "VM image for controller machine"
+  type = string
+  default = "projects/schedmd-slurm-public/global/images/family/schedmd-slurm-21-08-6-debian-10"
+}
 variable "controller_disk_type"    {}
 variable "controller_disk_size_gb" {}
 variable "controller_labels"       {}
 
 variable "login_machine_type" {}
 variable "login_node_count"   {}
-variable "login_image"        {}
+variable "login_image"        {
+  description = "VM image for login machine"
+  type = string
+  default = "projects/schedmd-slurm-public/global/images/family/schedmd-slurm-21-08-6-debian-10"
+}
 variable "login_disk_type"    {}
 variable "login_disk_size_gb" {}
 variable "login_labels"       {}
